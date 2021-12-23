48b358e1ae3eb41fc304aace8d2366b392c2675e
BuildTools <shyang2008@naver.com>
2021-12-23 18:23:01 +0900

    Github Actions 도입을 위한 2차 커밋

--- pat-api/src/test/kotlin/io/github/copecone/pat/util/TypeSupportTest.kt
+++ pat-api/src/test/kotlin/io/github/copecone/pat/util/TypeSupportTest.kt
@@ -8,7 +8,7 @@ class TypeSupportTest {
     @Test
     fun checkByteArrayAndHex() {
         val hexString = "d5edb2cf5552981adc42c03d87e459952cdd621a"
-        val byteArrayAnswer = byteArrayOf(-43, -19, -78, -49, 85, 82, -104, 26, -36, 66, -64, -121, -28, 89, -107, 44, -35, 98, 26)
+        val byteArrayAnswer = byteArrayOf(-43, -19, -78, -49, 85, 82, -104, 26, -36, 66, -64, 61, -121, -28, 89, -107, 44, -35, 98, 26)
 
         val byteArray = hexString.toByteArray()
         val hexStringRecover = byteArray.toHexString()
