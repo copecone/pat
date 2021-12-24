31f3c984a4e43bcebbde54d2061d6e9331495f88
BuildTools <controld3785@gmail.com>
2021-12-24 18:10:26 +0900

    TypeSupport KDoc 수정 Unpublish Action 테스트

--- pat-api/src/main/kotlin/io/github/copecone/pat/util/TypeSupport.kt
+++ pat-api/src/main/kotlin/io/github/copecone/pat/util/TypeSupport.kt
@@ -11,7 +11,7 @@ fun String.toByteArray(): ByteArray =
     HexFormat.of().parseHex(this)
 
 /**
- * [String]으로 저장된 헥스값을 [ByteArray]로 바꾸는 함수
+ * [ByteArray]을 [String] 헥스로 바꾸는 함수
  */
 fun ByteArray.toHexString(): String =
     BigInteger(1, this).toString(16)
